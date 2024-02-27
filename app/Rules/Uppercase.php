8a291bfd4edff5e3429665fec49103e724135e07
Xuan <108394451+xuanxuan27@users.noreply.github.com>
2024-02-27 16:05:29 +0800

    Create Uppercase.php

---
+++ app/Rules/Uppercase.php
@@ -0,0 +1,17 @@
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class Uppercase implements Rule
+{
+    public function passes($attribute, $value)
+    {
+        // 檢查標題的第一個字母是否為大寫
+        return ctype_upper(substr($value, 0, 1));
+    }
+
+    public function message()
+    {
+        return 'The :attribute does not start with an uppercased letter.';
+    }
+}
